8e42dee8fb69f9b98474147bc5082d71aef4549b
Maksim <m-zhitkov@inbox.ru>
2024-07-26 22:51:13 +0300

    Brought the code in accordance with PEP8

--- admin_settings/models.py
+++ admin_settings/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class SingletonModel(models.Model):
     """Модель нужна для того чтобы у нас в настройках была всегда одна запись"""
+
     class Meta:
         abstract = True
 
@@ -20,20 +21,36 @@ class SingletonModel(models.Model):
 
 class SiteSettings(SingletonModel):
     """Непосредственно сама модель с настройками"""
-    logotype = models.FileField(upload_to='files/logo', verbose_name='Логотип', default=0)
-    phone = models.CharField(max_length=15, verbose_name='Телефон', default=0)
-    email = models.EmailField(max_length=50, verbose_name='Email', default=0)
-    skype = models.CharField(max_length=50, verbose_name='Skype', default=0)
-    address = models.CharField(max_length=200, verbose_name='Адрес', default=0)
-    price_delivery_express = models.DecimalField(verbose_name='Стоимость экспресс доставки',
-                                                 max_digits=8, decimal_places=2, default=0)
-    price = models.DecimalField(verbose_name='Стоимость обычной доставки', max_digits=8, decimal_places=2, default=0)
-    min_sum = models.DecimalField(verbose_name='Минимальная сумма для бесплатной доставки',
-                                  max_digits=8, decimal_places=2, default=0)
+
+    logotype = models.FileField(
+        upload_to="files/logo", verbose_name="Логотип", default=0
+    )
+    phone = models.CharField(max_length=15, verbose_name="Телефон", default=0)
+    email = models.EmailField(max_length=50, verbose_name="Email", default=0)
+    skype = models.CharField(max_length=50, verbose_name="Skype", default=0)
+    address = models.CharField(max_length=200, verbose_name="Адрес", default=0)
+    price_delivery_express = models.DecimalField(
+        verbose_name="Стоимость экспресс доставки",
+        max_digits=8,
+        decimal_places=2,
+        default=0,
+    )
+    price = models.DecimalField(
+        verbose_name="Стоимость обычной доставки",
+        max_digits=8,
+        decimal_places=2,
+        default=0,
+    )
+    min_sum = models.DecimalField(
+        verbose_name="Минимальная сумма для бесплатной доставки",
+        max_digits=8,
+        decimal_places=2,
+        default=0,
+    )
 
     def __str__(self):
-        return 'Configuration'
+        return "Configuration"
 
     class Meta:
-        verbose_name = 'настройки сайта'
-        verbose_name_plural = 'настройки сайта'
+        verbose_name = "настройки сайта"
+        verbose_name_plural = "настройки сайта"
