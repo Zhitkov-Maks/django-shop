5d48b455b8c89c02fae51ec0a4bad5ac4e2cf364
Maksim <m-zhitkov@inbox.ru>
2024-07-26 22:48:52 +0300

    changed the tabs

--- admin_settings/context_processors.py
+++ admin_settings/context_processors.py
@@ -3,4 +3,4 @@ from .models import SiteSettings
 
 def load_settings(request):
     """Функция для получения настроек из базы и передачи их в шаблон"""
-    return {'site_settings': SiteSettings.load()}
+    return {"site_settings": SiteSettings.load()}
