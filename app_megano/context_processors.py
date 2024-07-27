f7c9e7faf780cbd644ef5d748363f7fb9d775895
Maksim <m-zhitkov@inbox.ru>
2024-07-27 10:51:09 +0300

    I changed the pads

--- app_megano/context_processors.py
+++ app_megano/context_processors.py
@@ -3,11 +3,11 @@ from orders.services.orderInfo import OrderInfo
 
 
 def cart(request):
-    return {'cart': Cart(request)}
+    return {"cart": Cart(request)}
 
 
 def load_order(request):
     order = OrderInfo(request)
     if list(order):
-        return {'order': list(order)[0]}
+        return {"order": list(order)[0]}
     return []
