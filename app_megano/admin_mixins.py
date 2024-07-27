00dde6673294b801022ed6d7f98587475dd15632
Maksim <m-zhitkov@inbox.ru>
2024-07-27 10:49:20 +0300

    Redesigned the class

--- app_megano/admin_mixins.py
+++ app_megano/admin_mixins.py
@@ -7,15 +7,20 @@ from django.db.models import QuerySet
 
 class ExportAsCSVMixin:
     def export_csv(self, request: HttpRequest, queryset: QuerySet):
+        """Метод реализующий экспорт csv файла."""
         meta: Options = self.model._meta
         field_names = [field.name for field in meta.fields]
 
         response = HttpResponse(content_type="text/csv")
-        response["Content-Disposition"] = f"attachment; filename={meta}-export.csv"
+        response["Content-Disposition"] = (f""
+                                           f"attachment; "
+                                           f"filename={meta}-export.csv"
+                                           )
 
         csv_writer = csv.writer(response)
         csv_writer.writerow(field_names)
         for obj in queryset:
             csv_writer.writerow([getattr(obj, field) for field in field_names])
         return response
+
     export_csv.shor_description = "Export as CSV"
