9c30f012b43493b8d369f6353f812a1aa844bf37
Maksim <m-zhitkov@inbox.ru>
2024-07-27 20:56:27 +0300

    Added annotation

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,4 @@
+from django.db.models import PositiveIntegerField
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.views.generic import ListView
@@ -11,59 +12,57 @@ from orders.services.payment import check_cart
 
 
 class CartView(ListView):
-    """Класс для отображения списка товаров в корзине пользователя. """
+    """Класс для отображения списка товаров в корзине пользователя."""
     model = Goods
-    template_name = 'cart/cart.html'
+    template_name: str = "cart/cart.html"
 
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data()
+    def get_context_data(self, *, object_list=None, **kwargs) -> dict:
+        """Добавляет в контекст корзину."""
+        context: dict = super().get_context_data()
         cart = Cart(self.request)
         stock = check_cart(cart)
-        context.update({'stock': stock, 'header': 'Корзина'})
+        context.update({"stock": stock, "header": "Корзина"})
         return context
 
 
 def add_product(request, pk, quantity=1):
     """Добавляет продукт в корзину."""
-    cart = Cart(request)
-    product = get_object_or_404(Goods, id=pk)
+    cart: Cart = Cart(request)
+    product: Goods = get_object_or_404(Goods, id=pk)
 
-    stock_in_store = product.stock
+    stock_in_store: PositiveIntegerField = product.stock
     product_true, quantity_in_cart = check_product_in_cart(cart, product)
+
     if stock_in_store <= quantity_in_cart:
-        return JsonResponse({'success': False})
+        return JsonResponse({"success": False})
+
     else:
-        cart.add(
-            product=product,
-            quantity=quantity
-        )
-        response = {'success': True}
+        cart.add(product=product, quantity=quantity)
+        response = {"success": True}
         return JsonResponse(response)
 
 
-def delete_product(request, pk):
+def delete_product(request, pk) -> JsonResponse:
     """Удаляет продукт из корзины."""
     cart = Cart(request)
 
     product = get_object_or_404(Goods, id=pk)
     cart.remove(product=product)
-    response = {'success': True}
+    response = {"success": True}
     return JsonResponse(response)
 
 
-def remove_product_quantity(request, pk):
+def remove_product_quantity(request, pk) -> JsonResponse:
     """Уменьшаем количество выбранного товара в корзине."""
-    cart = Cart(request)
+    cart: Cart = Cart(request)
 
-    product = get_object_or_404(Goods, id=pk)
-    cart.add(
-        product=product,
-        quantity=-1
-    )
-    response = {'success': True}
+    product: Goods = get_object_or_404(Goods, id=pk)
+    cart.add(product=product, quantity=-1)
+    response = {"success": True}
+
     for goods in cart:
-        if goods['product'] == product:
-            if goods['quantity'] <= 0:
-                cart.remove(product=goods['product'])
-                response = {'success': False}
+        if goods["product"] == product:
+            if goods["quantity"] <= 0:
+                cart.remove(product=goods["product"])
+                response = {"success": False}
     return JsonResponse(response)
