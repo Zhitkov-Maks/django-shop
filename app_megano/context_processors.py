55e896d05e591234adde0583f834957029214573
Maksim <m-zhitkov@inbox.ru>
2024-07-27 18:33:04 +0300

    Added annotation

--- app_megano/context_processors.py
+++ app_megano/context_processors.py
@@ -2,11 +2,15 @@ from cart.services.cart import Cart
 from orders.services.orderInfo import OrderInfo
 
 
-def cart(request):
+def cart(request) -> dict:
+    """Добавляем в контекст данные о корзине."""
     return {"cart": Cart(request)}
 
 
-def load_order(request):
+def load_order(request) -> dict | list:
+    """
+    Добавляем данные о заказе, нужно для вывода введенных пользователем данных.
+    """
     order = OrderInfo(request)
     if list(order):
         return {"order": list(order)[0]}
