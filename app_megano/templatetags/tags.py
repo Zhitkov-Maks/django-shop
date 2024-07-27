5cd10b4c0ce383c3aa8deb7e89aa889db9d1df54
Maksim <m-zhitkov@inbox.ru>
2024-07-27 09:57:28 +0300

    Formatting the code

--- app_megano/templatetags/tags.py
+++ app_megano/templatetags/tags.py
@@ -6,26 +6,32 @@ from app_megano.models import Category, Tags, Goods
 register = template.Library()
 
 
-@register.simple_tag(name='get_cats')
+@register.simple_tag(name="get_cats")
 def all_categories() -> list:
     """Функция возвращает список категорий"""
-    return Category.objects.filter(active=True).order_by('name')
+    return Category.objects.filter(active=True).order_by("name")
 
 
-@register.simple_tag(name='get_tags')
+@register.simple_tag(name="get_tags")
 def load_tag() -> dict:
-    """Функция для вывода тегов в catalog.html. Сделал так как уже минимум в трех местах был повторяющийся код."""
-    tags = Tags.objects.annotate(Count('tags')).order_by('-tags__count')[:10]
-    return tags
+    """
+    Функция для вывода тегов в catalog.html. Сделал так как уже минимум в
+    трех местах был повторяющийся код.
+    """
+    return Tags.objects.annotate(Count("tags")).order_by("-tags__count")[:10]
 
 
-@register.simple_tag(name='get_priceMin')
+@register.simple_tag(name="get_priceMin")
 def price_min() -> dict:
-    """Функция для возврата минимальной цены для страницы с каталогом товаров"""
-    return Goods.objects.aggregate(Min('price'))
+    """
+    Функция для возврата минимальной цены для страницы с каталогом товаров.
+    """
+    return Goods.objects.aggregate(Min("price"))
 
 
-@register.simple_tag(name='get_priceMax')
+@register.simple_tag(name="get_priceMax")
 def price_max() -> dict:
-    """Функция для возврата максимальной цены для страницы с каталогом товаров"""
-    return Goods.objects.aggregate(Max('price'))
+    """
+    Функция для возврата максимальной цены для страницы с каталогом товаров.
+    """
+    return Goods.objects.aggregate(Max("price"))
