d39dd8accb044e14a5091e1d0cc5b99d6986e3d7
Maksim <m-zhitkov@inbox.ru>
2024-07-28 12:30:00 +0300

    General code improvements

--- orders/services/orderInfo.py
+++ orders/services/orderInfo.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 from unicodedata import decimal
 
 from django.conf import settings
@@ -8,23 +9,32 @@ from orders.models import Order
 
 
 class OrderInfo(object):
-    """Класс для хранения информации о заказе в сессии"""
+    """
+    Класс для хранения информации о заказе в сессии. Нужен для при
+    оформлении заказа, чтобы вывести введенные пользователем данные.
+    """
 
     def __init__(self, request):
-        """Инициализируем заказ"""
+        """Инициализируем заказ."""
         self.session = request.session
-        order = self.session.get(settings.ORDER_SESSION_INFO)
+        order: dict = self.session.get(settings.ORDER_SESSION_INFO)
+
         if not order:
             order = self.session[settings.ORDER_SESSION_INFO] = {}
         self.order = order
 
-    def add(self, form, request):
-        """Добавить заказ."""
-        type_delivery = Order.get_delivery(form.cleaned_data.get("type_delivery"))
-        type_payment = Order.get_payment(form.cleaned_data.get("type_payment"))
+    def add(self, form, request) -> None:
+        """Добавить заказ в словарь из сессии."""
+        type_delivery: str = Order.get_delivery(
+            form.cleaned_data.get("type_delivery")
+        )
+        type_payment: str = Order.get_payment(
+            form.cleaned_data.get("type_payment")
+        )
         delivery_price, total_price = get_delivery_price(
             form.cleaned_data.get("type_delivery"), request
         )
+
         self.order["order"] = {
             "full_name": form.cleaned_data.get("full_name"),
             "city": form.cleaned_data.get("city"),
@@ -42,28 +52,38 @@ class OrderInfo(object):
         self.session[settings.ORDER_SESSION_INFO] = self.order
         self.session.modified = True
 
-    def get_total_price(self):
+    def get_total_price(self) -> int:
+        """Получаем общую сумму заказа."""
         return self.order["order"]["total_price"]
 
     def __iter__(self):
         """Перебор элементов в заказе и получение информации из сессии."""
-        order = self.order
+        order: dict = self.order
         for item in order:
             item = order[item]
             yield item
 
 
-def get_delivery_price(type_delivery, request):
-    """Функция для получения стоимости доставки, в зависимости от суммы и выбранной доставки."""
-    price = 0
-    admin_settings = SiteSettings.objects.all()[0]
-    cart = Cart(request)
+def get_delivery_price(type_delivery, request) -> Tuple[float, float]:
+    """
+    Функция для получения стоимости доставки, в зависимости от суммы
+    и выбранной доставки.
+    """
+    price: int = 0
+
+    # Получаем стоимость доставки
+    admin_settings: SiteSettings = SiteSettings.objects.all()[0]
+    cart: Cart = Cart(request)
+
     if type_delivery == "express":
         price = admin_settings.price_delivery_express
+
     elif type_delivery == "simple":
         price = admin_settings.price
-        min_sum = admin_settings.min_sum
+        min_sum: int = admin_settings.min_sum
+
         if cart.get_total_price() >= min_sum:
             price = decimal("0")
-    total_price = cart.get_total_price() + price
+
+    total_price: int = cart.get_total_price() + price
     return float(price), float(total_price)
