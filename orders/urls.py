e9590827119f233ebe05cdfab645b07b47ff6b0c
Maksim <m-zhitkov@inbox.ru>
2024-07-28 12:58:55 +0300

    Improving code readability

--- orders/urls.py
+++ orders/urls.py
@@ -14,10 +14,20 @@ urlpatterns = [
     path("", OrderView.as_view(), name="order"),
     path("payment/<int:pk>/", PaymentView.as_view(), name="payment"),
     path(
-        "paymentsomeone/<int:pk>/", PaymentSomeOneView.as_view(), name="paymentSomeOne"
+        "paymentsomeone/<int:pk>/",
+        PaymentSomeOneView.as_view(),
+        name="paymentSomeOne"
     ),
-    path("progresspayment/", ProgressPaymentView.as_view(), name="progressPayment"),
-    path("oneorder/<int:pk>", OneOrderView.as_view(), name="oneOrder"),
+    path(
+        "progresspayment/",
+        ProgressPaymentView.as_view(),
+        name="progressPayment"
+    ),
+    path(
+        "oneorder/<int:pk>",
+        OneOrderView.as_view(),
+        name="oneOrder"
+    ),
     path("modalLogin/", login_modal, name="modalLogin"),
     path("addInfoUser/", add_info_about_user, name="addInfo"),
 ]
