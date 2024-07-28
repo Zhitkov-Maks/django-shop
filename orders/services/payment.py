1ff579ea13402ae706b5ee75d0d4bf45655eec26
Maksim <m-zhitkov@inbox.ru>
2024-07-28 12:45:07 +0300

    General code improvements

--- orders/services/payment.py
+++ orders/services/payment.py
@@ -1,27 +1,40 @@
 import random
+from typing import Tuple
 
 from django.db import transaction
+from django.db.models.fields import PositiveIntegerField
+from django.http import HttpRequest
 
-from orders.models import Status, DetailOrder
+from app_users.models import CustomUser
+from orders.forms import OrderForms
+from orders.models import Status, DetailOrder, Order
 from cart.services.cart import Cart
 from app_megano.models import Purchases, Goods
 
 
-def add_order(form, user, total_price) -> tuple:
-    """Добавляем информацию о заказе. Возвращаем заказ и тип платежа чтобы в зависимости от выбранного платежа
-    перенаправить на нужную страницу"""
-    order = form.save(commit=False)
+def add_order(
+        form: OrderForms,
+        user: CustomUser,
+        total_price: int
+) -> Tuple[Order, str]:
+    """
+    Добавляем информацию о заказе. Возвращаем заказ и тип платежа чтобы в
+    зависимости от выбранного платежа перенаправить на нужную страницу.
+    """
+    order: Order = form.save(commit=False)
+
     order.total_price = total_price
     order.user_id = user.id
-    order.status = Status.objects.get(pk=2)
+    order.status = Status.objects.get(id=2)
+
     order.save()
     return order, order.type_payment
 
 
 @transaction.atomic
-def add_detail_to_order(order, request) -> None:
+def add_detail_to_order(order: Order, request: HttpRequest) -> None:
     """Добавляем к заказу товары из которых он состоит."""
-    cart = Cart(request)
+    cart: Cart = Cart(request)
     for item in cart:
         DetailOrder.objects.create(
             order=order,
@@ -29,27 +42,35 @@ def add_detail_to_order(order, request) -> None:
             price=item["price"],
             quantity=item["quantity"],
         )
-        Purchases.objects.create(goods=item["product"], quantity=item["quantity"])
-        product = Goods.objects.get(id=item["product"].id)
+
+        Purchases.objects.create(
+            goods=item["product"],
+            quantity=item["quantity"]
+        )
+        product: Goods = Goods.objects.get(id=item["product"].id)
         product.stock -= item["quantity"]
-        # Проверяем не закончился ли товар, если закончился то переводим в статус не активен
-        if product.stock <= 0:
+
+        # Проверяем не закончился ли товар, если закончился то
+        # переводим в статус не активен
+        if product.stock == 0:
             product.is_active = False
         product.save()
+
+    # Очищаем корзину
     cart.clear()
 
 
-def check_cart(cart) -> bool:
+def check_cart(cart: Cart) -> bool:
     """Проверяем не закончился ли товар, который есть в корзине."""
-    stock = True
+    stock: bool = True
     for item in cart:
-        product = Goods.objects.get(id=item["product"].id)
+        product: Goods = Goods.objects.get(id=item["product"].id)
         if not product.is_active:
             stock = False
     return stock
 
 
-def get_number_card(form, order):
+def get_number_card(form: OrderForms, order: Order):
     """Проверяем номер карты на валидность условиям."""
     list_payment_failed = [
         "Недостаточно средств",
@@ -58,12 +79,17 @@ def get_number_card(form, order):
         "Подозрение на спам",
         "Слишком часто заказываете у нас",
     ]
+
     # Проверяем номер карты на соответствие условиям платежа
     card_number = int(
-        "".join([item for item in form.cleaned_data.get("number") if item.isdigit()])
+        "".join([
+            item for item in form.cleaned_data.get("number") if item.isdigit()
+        ])
     )
+
     if card_number % 10 == 0 or card_number % 10 == 6:
         order.comment = random.choice(list_payment_failed)
+
     else:
         order.status = Status.objects.get(pk=1)
         order.paid = True
