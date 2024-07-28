d73ee268db41af4f2d3df68016122b40ea17a0ee
Maksim <m-zhitkov@inbox.ru>
2024-07-28 11:48:39 +0300

    Split a large function into several small ones

--- app_users/services.py
+++ app_users/services.py
@@ -1,22 +1,21 @@
-"""Модуль для проверки введенных данных редактирования профиля, и сохранения."""
-
+"""
+Модуль для проверки введенных данных редактирования профиля, и сохранения.
+"""
 from django.contrib.auth import authenticate, login
+from django.http import HttpRequest
 
+from .forms import UpdateUserForm
 from .models import Profile, CustomUser
 
 
-def func_for_check_form(form, user, request) -> bool:
-    """Проверяем форму и сохраняем данные из формы. Если редактирование прошло успешно, то возвращаем True
-    (В конце функции проверяем на is_valid, это срабатывает так как при возникновении каких либо ошибок мы
-    добавляем к форме add_error), если нет, то False"""
-    edit_email(user, form.cleaned_data.get("email"), form)  # Проверка email
-    user.first_name = form.cleaned_data.get("full_name").split()[1]
-    user.last_name = form.cleaned_data.get("full_name").split()[0]
-
+def update_profile_data(user: CustomUser, form: UpdateUserForm) -> None:
+    """Функция для обновления модели профиля."""
     if hasattr(user, "profile"):
-        edit_phone(user, form.cleaned_data.get("phone"), form)  # Проверяем телефон
+        edit_phone(user, form.cleaned_data.get("phone"), form)
+
         if form.cleaned_data.get("avatar"):
             user.profile.photo = form.cleaned_data.get("avatar")
+
         user.profile.patronymic = form.cleaned_data.get("full_name").split()[2]
         user.profile.save()
 
@@ -27,45 +26,94 @@ def func_for_check_form(form, user, request) -> bool:
             photo=form.cleaned_data["avatar"],
             phone=form.cleaned_data["phone"],
         )
+
+
+def update_password(
+        user: CustomUser,
+        form: UpdateUserForm,
+        request: HttpRequest
+) -> None:
+    """Функция для проверки и сохранения паролей."""
     if form.cleaned_data.get("password") and form.cleaned_data.get(
         "passwordReplay"
-    ):  # Если были введены пароли
-        if form.cleaned_data["password"] == form.cleaned_data["passwordReplay"]:
-            password = form.cleaned_data.get("password")
+    ):
+        password = form.cleaned_data.get("password")
+        # Если были введены пароли
+        if password == form.cleaned_data["passwordReplay"]:
+
             user.set_password(password)
             user.save()
+
             # Если смена пароля прошла, то нужно снова авторизоваться
-            email = form.cleaned_data.get("email")
-            raw_pass = form.cleaned_data.get("password")
-            user = authenticate(email=email, password=raw_pass)
+            user = authenticate(
+                email=form.cleaned_data.get("email"),
+                password=password
+            )
             login(request, user)
+
         else:
             form.add_error("passwordReplay", "Пароли не совпадают.")
-            form["passwordReplay"].field.widget.attrs["class"] += " form-input_error"
+            form[
+                "passwordReplay"
+            ].field.widget.attrs["class"] += ' form-input_error'
+
+
+def distributor_function(form, user, request) -> bool:
+    """
+    Проверяем форму и сохраняем данные из формы. Если редактирование прошло
+    успешно, то возвращаем True (В конце функции проверяем на is_valid,
+    это срабатывает так как при возникновении каких либо ошибок мы
+    добавляем к форме add_error), если нет, то False.
+    """
+    # Сначала отправляем на проверку и сохранение email
+    edit_email(user, form.cleaned_data.get("email"), form)
+
+    # Сохраняем имя и фамилию
+    user.first_name = form.cleaned_data.get("full_name").split()[1]
+    user.last_name = form.cleaned_data.get("full_name").split()[0]
+
+    # Отправляем на обновление данных в профиле
+    update_profile_data(user, form)
+
+    # Отправляем на обновление пароля
+    update_password(user, form, request)
+
+    # Сохраняем все что обновили
     user.save()
+
     if form.is_valid():
         return True
     return False
 
 
-def edit_email(user, email, form):
-    """Функция для сохранения email, если пользователь хочет сменить email проверяем не используется ли уже данная
-    почта."""
+def edit_email(user: CustomUser, email: str, form: UpdateUserForm):
+    """
+    Функция для сохранения email, если пользователь хочет сменить email
+    проверяем не используется ли уже данная почта.
+    """
     if email != user.email:
         if CustomUser.objects.filter(email=email).exists():
             form.add_error("email", "Данный email уже используется")
-            form["email"].field.widget.attrs["class"] += " form-input_error"
+            form[
+                "email"
+            ].field.widget.attrs["class"] += " form-input_error"
+
         else:
             user.email = email
             user.save()
 
 
-def edit_phone(user, phone, form):
-    """Функция для проверки и сохранения измененного номера телефона. Проверка на существование введенного телефона"""
+def edit_phone(user: CustomUser, phone: str, form: UpdateUserForm):
+    """
+    Функция для проверки и сохранения измененного номера телефона.
+    Проверка на существование введенного телефона.
+    """
     if phone != user.profile.phone:
         if Profile.objects.filter(phone=phone).exists():
             form.add_error("phone", "Этот телефон уже используется")
-            form["phone"].field.widget.attrs["class"] += " form-input_error"
+            form[
+                "phone"
+            ].field.widget.attrs["class"] += " form-input_error"
         else:
             user.profile.phone = phone
             user.profile.save()
