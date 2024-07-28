3c5afec6787f24d91791a419288d7489ef2e2a18
Maksim <m-zhitkov@inbox.ru>
2024-07-28 11:15:48 +0300

    Overall code improvement

--- app_users/models.py
+++ app_users/models.py
@@ -1,17 +1,24 @@
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models import OneToOneField, ImageField
+from django.db.models.fields import EmailField, CharField
 
 from .usermanager import CustomUserManager
 
 
 class CustomUser(AbstractUser):
-    """Наследуемся от AbstractUser для того, чтобы регистрировать пользователя по email."""
+    """
+    Наследуемся от AbstractUser для того, чтобы регистрировать
+    пользователя по email.
+    """
 
-    username = None
-    email = models.EmailField("email address", unique=True)
+    username: None = None
+    email: EmailField = models.EmailField(
+        "email address", unique=True
+    )
 
-    USERNAME_FIELD = "email"
+    USERNAME_FIELD: str = "email"
     REQUIRED_FIELDS = []
 
     objects = CustomUserManager()
@@ -21,14 +28,23 @@ class CustomUser(AbstractUser):
 
 
 class Profile(models.Model):
-    """Расширяем стандартную таблицу пользователя для добавления телефона и аватарки."""
+    """
+    Расширяем стандартную таблицу пользователя для добавления
+    телефона и аватарки.
+    """
 
-    user = models.OneToOneField(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Пользователь"
+    user: OneToOneField = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name="Пользователь"
     )
-    patronymic = models.CharField(max_length=20, verbose_name="Отчество")
-    photo = models.ImageField(upload_to="files/image", verbose_name="Фото", blank=True)
-    phone = models.CharField(max_length=20, verbose_name="Телефон")
+    patronymic: CharField = models.CharField(
+        max_length=20, verbose_name="Отчество"
+    )
+    photo: ImageField = models.ImageField(
+        upload_to="files/image", verbose_name="Фото", blank=True
+    )
+    phone: CharField = models.CharField(max_length=20, verbose_name="Телефон")
 
     def __str__(self):
         return str(self.user)
